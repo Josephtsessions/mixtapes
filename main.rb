d08de2fe04a263a4b6d7aae409ce2ed71acb217b
Joseph Sessions <Josephtsessions@gmail.com>
2019-09-11 23:54:11 -0700

    Adds functionality for the other changes to main.rb

--- main.rb
+++ main.rb
@@ -46,22 +46,46 @@ begin
     errors = []
     
     case change["type"]
-      when ADD_SONG
-        playlist_id = change["playlist_id"]
-        song_id = change["song_id"]
-        
-        playlist = playlists.detect {|playlist| playlist.id == playlist_id}
+    when ADD_SONG
+      playlist_id = change["playlist_id"]
+      song_id = change["song_id"]
+      
+      playlist = playlists.detect {|playlist| playlist.id == playlist_id}
+      song = songs.detect {|song| song.id == song_id}
+      
+      errors.push("Couldn't find song with id #{song_id}") if song.nil?
+      errors.push("Couldn't find playlist with id #{playlist_id}") if playlist.nil?
+      
+      if errors.empty?
+        playlist.add_song(song.id)
+      end
+    when CREATE_PLAYLIST
+      song_ids = change["song_ids"]
+      user_id = change["user_id"]
+      
+      song_ids.each do |song_id|
         song = songs.detect {|song| song.id == song_id}
-        
-        errors.push("Couldn't find song with id #{song_id}") if song.nil?
-        errors.push("Couldn't find playlist with id #{playlist_id}") if playlist.nil?
-        
-        if errors.empty?
-          playlist.add(song)
-        end
-      when CREATE_PLAYLIST
-        
-      when REMOVE_PLAYLIST
+
+        errors.push("Couldn't find song ##{song_id}") if song.nil?
+      end
+
+      user = users.detect {|user| user.id == user_id}
+      
+      if errors.empty?
+        last_playlist_id = playlists.map(&:id).sort&.max || 0
+        new_playlist = Playlist.new(id: last_playlist_id.to_i + 1, user_id: user_id, song_ids: song_ids)
+        playlists.push()
+      end
+    when REMOVE_PLAYLIST
+      playlist_id = change["playlist_id"]
+      
+      playlist_index = playlists.find_index {|playlist| playlist.id == playlist_id}
+      
+      errors.push("Couldn't find playlist ##{playlist.id}") if playlist_index.nil?
+      
+      if errors.empty?
+        playlists.delete_at(playlist_index)
+      end
     end
     
     # I chose to skip here - I'd rather skip a bad row than fail the whole operation for the user. Skipping doesn't
