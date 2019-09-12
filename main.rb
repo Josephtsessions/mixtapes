24911ba5a04d0a00ed980ba3ee8dfbd7906affc3
Joseph Sessions <Josephtsessions@gmail.com>
2019-09-11 23:35:58 -0700

    Adds "add a song to a playlist" code and changes.json
    Add a song won't work until I've added that behavior to the model, but this starts to form what I'll need for that so I can start to identify the patterns and how I wanna break the code out.

--- main.rb
+++ main.rb
@@ -14,6 +14,10 @@ INPUT_FILENAME = ARGV[0]
 CHANGES_FILENAME = ARGV[1]
 OUTPUT_FILENAME = ARGV[2]
 
+ADD_SONG = "add song"
+CREATE_PLAYLIST = "create playlist"
+REMOVE_PLAYLIST = "remove playlist"
+
 # We should check for an existing file before blindly writing to it so bad things don't happen!
 if File.exist?(OUTPUT_FILENAME)
   puts "#{OUTPUT_FILENAME} already exists. Please specify a new filename or move the file so you don't lose data!"
@@ -29,8 +33,47 @@ begin
   songs = Song.from_json(input_json)
   users = User.from_json(input_json)
 rescue JSON::ParserError => e
-   puts "#{input_filename} doesn't appear to be properly formatted JSON." 
+   puts "#{INPUT_FILENAME} doesn't appear to be properly formatted JSON." 
    raise e
+end
+
+begin
+  changes_file = File.read(CHANGES_FILENAME)
+  
+  changes = JSON.parse(changes_file).fetch("changes", [])
+  
+  changes.each do |change|
+    errors = []
+    
+    case change["type"]
+      when ADD_SONG
+        playlist_id = change["playlist_id"]
+        song_id = change["song_id"]
+        
+        playlist = playlists.detect {|playlist| playlist.id == playlist_id}
+        song = songs.detect {|song| song.id == song_id}
+        
+        errors.push("Couldn't find song with id #{song_id}") if song.nil?
+        errors.push("Couldn't find playlist with id #{playlist_id}") if playlist.nil?
+        
+        if errors.empty?
+          playlist.add(song)
+        end
+      when CREATE_PLAYLIST
+        
+      when REMOVE_PLAYLIST
+    end
+    
+    # I chose to skip here - I'd rather skip a bad row than fail the whole operation for the user. Skipping doesn't
+    # make sense for every business problem but it does for this one and I think it's more gracious this way.
+    if !errors.empty?
+      puts "Skipping #{change}:"
+      errors.each {|error| puts "    #{error}"}
+    end
+  end
+rescue JSON::ParserError => e
+  puts "#{CHANGES_FILENAME} doesn't appear to be properly formatted JSON."
+  raise e
 end
 
 output_json = {
